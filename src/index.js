aab69a1d890df74798c1e38ed83b08c102cbd35f
clay.zenx <clay.zenx@gmail.com>
2020-06-01 15:13:31 +0700

    Finish base framwork functional

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import './module'
 import './scss/index.scss'
+import {Excel} from '@/components/excel/Excel';
+import {Header} from '@/components/header/Header';
+import {Table} from '@/components/table/Table';
+import {Formula} from '@/components/formula/Formula';
+import {Toolbar} from '@/components/toolbar/Toolbar';
 
-console.log('asd')
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+})
+
+excel.render();
 
 
